379ace722c9664fc53686fe80259b1ed2eb1d886
supriyo <hisupriyooo@gmail.com>
2020-08-24 15:46:54 +0530

    Updates

---
+++ precache-manifest.77706d2e29a4069129ae1982e9be4950.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873c059396e48bed91fe118f109d1dbd",
+    "url": "/cocktail_maker/index.html"
+  },
+  {
+    "revision": "15808831a45f8314c1da",
+    "url": "/cocktail_maker/static/css/main.474e8452.chunk.css"
+  },
+  {
+    "revision": "4cb06177203adbccd574",
+    "url": "/cocktail_maker/static/js/2.115589cf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/cocktail_maker/static/js/2.115589cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15808831a45f8314c1da",
+    "url": "/cocktail_maker/static/js/main.95ea2435.chunk.js"
+  },
+  {
+    "revision": "8c56fe893eb704a68884",
+    "url": "/cocktail_maker/static/js/runtime-main.ddd67abc.js"
+  },
+  {
+    "revision": "a928df949403aff115e2ec4822373ce6",
+    "url": "/cocktail_maker/static/media/cocktail.a928df94.png"
+  },
+  {
+    "revision": "034d0b6ebebdd40a37b53752407fedbb",
+    "url": "/cocktail_maker/static/media/liquor.034d0b6e.png"
+  }
+]);
